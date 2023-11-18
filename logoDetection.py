0cef43687c7ce7da841907f228e4b6affd7aaa3e
vickypedia-12 <vikasmourya54321@gmail.com>
2023-11-18 15:06:18 +0530

    Relative path

--- logoDetection.py
+++ logoDetection.py
@@ -1,9 +1,9 @@
 from google.cloud import vision_v1p2beta1 as vision 
 from google.oauth2 import service_account
-import os
+# import os
 def detect_logo(file_path):
     # keypath = os.path.join('D:','codes','LogoDetection','logo-detection-system-811c426b1dea.json')
-    keypath = 'd:/codes/LogoDetection/logo-detection-system-811c426b1dea.json'
+    keypath = 'logo-detection-system.json'
     credentials = service_account.Credentials.from_service_account_file(keypath)
     client = vision.ImageAnnotatorClient(credentials=credentials)
     
@@ -23,5 +23,5 @@ def detect_logo(file_path):
             print("No logos Detected")
             
 # My_api_key = "AIzaSyAu0whNgg3KE0ifp4nT4jfPe3Cd7Ui9D54"
-detect_logo('D:/codes/LogoDetection/kfc.png')
-detect_logo('D:/codes/LogoDetection/McD.jpg')
+detect_logo('kfc.png')
+# detect_logo('D:/codes/LogoDetection/McD.jpg')
